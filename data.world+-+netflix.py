a1c400dcc7c1584d02acebe9ff75f46f18c06f10
Jennifer Tsou <jennifertsou93@gmail.com>
2017-08-06 19:46:35 -0700

    Update data.world+-+netflix.py
    updated code to better understand the dataset

--- data.world+-+netflix.py
+++ data.world+-+netflix.py
@@ -9,44 +9,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 get_ipython().magic(u'matplotlib inline')
 
-
-# In[59]:
-
 df = pd.read_excel(r"C:\Users\jenni\Box Sync\Dataset\netflix.xlsx")
 
-
-# In[60]:
-
-df.head(1)
+#to see snipet of the code
+df.head()
 
 
-# In[61]:
 
 #Provide overview of the rating
-
 df.groupby(['rating']).describe()
 
-
-# In[62]:
 
 #Show what each rating stands for
 df[['rating','ratingLevel']].drop_duplicates()
 
-
-# In[72]:
-
 df.groupby(['rating']).count()
 
-
-# In[63]:
 
 #netflix show by rating
 df[['rating', 'release year']].groupby('release year').describe()#['release year'].describe()
 
 
-# In[88]:
 
-#set size for the plot
+#plot
 fig, ax = plt.subplots()
 fig.set_size_inches(16, 12)
 sns.barplot(x='release year', y ='user rating score', hue='rating', data=df)
@@ -54,7 +39,6 @@ plt.grid()
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 
-# In[ ]:
 
 
 
